64d2d15368d772a4fc2b4ad1c545790f21861d30
wujiayong <wb.wujiayong18@mesg.corp.netease.com>
2018-11-22 15:36:06 +0800

    群右键修改

--- src/renderer/components/chat/ChatNotice.vue
+++ src/renderer/components/chat/ChatNotice.vue
@@ -241,7 +241,7 @@ export default {
       if (event.button === 2) {
         if (this.power === 'owner') { // 管理员
           key = 'owner-member-manager'
-        } else if (this.power === 'normal') { // 普通群成员
+        } else if (this.power === 'normal' || this.power === 'manager') { // 普通群成员
           key = 'normal-member-manager'
         }
         this.$store.dispatch('showListOptions', {
