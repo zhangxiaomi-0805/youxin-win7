d92444f680fc914fb7a26bfd232905ddab58e53f
Zhangxj <zhangxj@hiwitech.com>
2018-11-26 10:52:04 +0800

    更新后，历史消息记录入口注释了，现在放开

--- src/renderer/components/chat/Chat.vue
+++ src/renderer/components/chat/Chat.vue
@@ -3,7 +3,7 @@
   <div class="g-hbf-header m-header">
     <span class="session-name">{{sessionName}}</span>
     <div class="m-setting" v-if="funKey >= 2 && teamInvalid && valid">
-      <!-- <a class="b-check" @click="showHistoryMsgModal"/> -->
+      <a class="b-check" @click="showHistoryMsgModal"/>
       <a class="b-add" v-if="funKey <= 2" @click="createTeam()"/>
       <a class="b-more" @click="openSliderMenu"/>
     </div>
